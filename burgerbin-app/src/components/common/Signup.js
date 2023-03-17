d66b46df9b72bea9caddea2222957c3b83142743
Michael McNiel <michaelmcniel65@gmail.com>
2023-03-16 20:55:28 -0500

    Added comment on design to signup.js

--- burgerbin-app/src/components/common/Signup.js
+++ burgerbin-app/src/components/common/Signup.js
@@ -28,3 +28,5 @@ export default function BasicTextFields({title, setPassword, setEmail, handleAct
         </div>
     );
 }
+
+{/* Need to design sign up page, then redirect to a profile setup page (i.e. username, picture, etc...) */}

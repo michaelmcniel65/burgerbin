5441f99028f3c892dcd70c2513ea1ee068ec4b4b
Michael McNiel <michaelmcniel65@gmail.com>
2023-03-19 20:00:11 -0500

    Set up basic profile builder configuration

--- burgerbin-app/src/components/ProfilePage.js
+++ burgerbin-app/src/components/ProfilePage.js
@@ -1,7 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { getAuth } from "firebase/auth";
 
 export default function ProfilePage() {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    const [name, setName] = useState('Anon');
+    const [proPic, setProPic] = useState('https://www.canbind.ca/canbindwebsitetest/wp-content/uploads/2018/04/blank-profile-picture-973460_640-300x300.png')
+    const displayName = name;
+    const email = user.email;
+    const photoURL = proPic;
+
     let navigate = useNavigate();
     useEffect(() => {
         let authToken = sessionStorage.getItem('Auth Token')
@@ -23,6 +32,9 @@ export default function ProfilePage() {
         <div>
             Profile
             <button onClick={handleLogout}>Log out</button>
+            <img className="w-auto h-auto" src={photoURL}/>
+            <h1>{displayName}</h1>
+            <h1>{email}</h1>
         </div>
     )
 }

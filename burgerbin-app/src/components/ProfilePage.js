d2a4f31160ac835740075a00fb65acb232df7d94
Michael McNiel <michaelmcniel65@gmail.com>
2023-03-20 18:39:54 -0500

    Worked on firebase updateprofile function

--- burgerbin-app/src/components/ProfilePage.js
+++ burgerbin-app/src/components/ProfilePage.js
@@ -2,15 +2,16 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getAuth, updateProfile } from "firebase/auth";
 import Navbar from "./common/Navbar";
+import { toast } from "react-toastify";
 
 export default function ProfilePage() {
     const auth = getAuth();
     const user = auth.currentUser;
-    const [name, setName] = useState('Anon');
+    const [name, setName] = useState();
     const [proPic, setProPic] = useState('https://www.canbind.ca/canbindwebsitetest/wp-content/uploads/2018/04/blank-profile-picture-973460_640-300x300.png')
-    const displayName = name;
+    const displayName = user.displayName;
     const email = user.email;
-    const photoURL = proPic;
+    const photoURL = user.photoURL;
 
     let navigate = useNavigate();
     useEffect(() => {
@@ -29,6 +30,20 @@ export default function ProfilePage() {
         navigate('/login')
     }
 
+    async function handleUpdate(event) {
+        try {
+            event.preventDefault();
+            await updateProfile(auth.currentUser, {
+                displayName: name, photoURL: proPic
+            });
+            return console.log(displayName)
+        } catch(error) {
+            if(error.code) {
+                toast.error('An error has occurred.')
+            }
+        }
+    }
+
     return (
         <div>
             <Navbar />
@@ -37,6 +52,9 @@ export default function ProfilePage() {
             <img className="w-auto h-auto" src={photoURL}/>
             <h1>{displayName}</h1>
             <h1>{email}</h1>
+            <input type="text" placeholder="Username" onChange={e => setName(e.target.value)} />
+            <button onClick={handleUpdate}>Submit</button>
+            <button onClick={() => navigate('/home')}>go home</button>
         </div>
     )
 }

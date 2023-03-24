e620373111e80af6b2b111d774adccff23b2effa
Michael McNiel <michaelmcniel65@gmail.com>
2023-03-23 22:38:45 -0500

    Added comment

--- burgerbin-app/src/components/common/ImageOptions.js
+++ burgerbin-app/src/components/common/ImageOptions.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function ImageOptions() {
     return (
         <div>
-            {/* finished part 1 need to watch part 2 codelikepro*/}
+            {/* finished part 1 need to watch part 2 codelikepro sorry schoomb got lost today not coding to take care of him*/}
         </div>
     )
 }
